6f276471a784d9822efda02c64e433156c44798d
calimanco <calimanco@qq.com>
2021-01-16 16:52:57 +0800

    chore: update Rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
 import pkg from './package.json'
 
 const libraryName = '--libraryname--'
@@ -27,14 +27,12 @@ export default {
   plugins: [
     // Allow json resolution
     json(),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-    // Resolve source maps to the original source
-    sourceMaps(),
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
     // Compile TypeScript files
     typescript({
       useTsconfigDeclarationDir: true,
@@ -45,6 +43,8 @@ export default {
           module: 'ES2015'
         }
       }
-    })
+    }),
+    // Resolve source maps to the original source
+    sourceMaps()
   ]
 }
