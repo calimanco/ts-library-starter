35da531fceaa2910ff09ba07583cbaa0d81dbad3
calimanco <calimanco@qq.com>
2021-01-18 14:14:12 +0800

    fix: fix lodash load error

--- init/common.ts
+++ init/common.ts
@@ -1,6 +1,6 @@
 import * as path from 'path'
 import * as fs from 'fs'
-import kebabCase from 'lodash.kebabcase'
+import { kebabCase } from 'lodash'
 import { defaultLang } from './config'
 
 export interface LangPkg {
