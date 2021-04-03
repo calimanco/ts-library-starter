fa9b2f270f2fdde4308ed0205b9dbfff36d9acfd
calimanco <calimanco@qq.com>
2021-04-03 19:18:00 +0800

    fix: change declarations folder name

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+/**
+ * It is recommended that this file is only used to expose declarations and the main entrance.
+ */
 import DummyClass from './--libraryname--'
 
 // export types
-export * from './types'
+export * from './declarations'
 
 // export main
 export default DummyClass
