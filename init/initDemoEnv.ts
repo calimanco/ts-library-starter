30c191bdf35eee2e58a63d7472d95413b1215d56
calimanco <calimanco@qq.com>
2021-06-04 16:13:26 +0800

    fix: eslint bug of demo

--- init/initDemoEnv.ts
+++ init/initDemoEnv.ts
@@ -1,4 +1,4 @@
-import { mkdirSync } from 'fs'
+import { mkdirSync, readFileSync, writeFileSync } from 'fs'
 import { execSync } from 'child_process'
 import { join } from 'path'
 import chalk from 'chalk'
@@ -14,6 +14,10 @@ export default async function initDemoEnv(): Promise<boolean> {
       'npm i play-anywhere --save-dev --no-package-lock  --no-audit'
     ).toString()
     mkdirSync(join(__dirname, '..', 'demos'))
+    const jsonTsConfig = join(__dirname, '..', 'tsconfig.eslint.json')
+    const tsConfig = JSON.parse(readFileSync(jsonTsConfig).toString())
+    tsConfig.include.push('demos')
+    writeFileSync(jsonTsConfig, JSON.stringify(tsConfig, null, 2) + '\n')
     console.group(chalk.underline(getLang(31)))
     console.log(chalk.green(getLang(28)))
     console.groupEnd()
